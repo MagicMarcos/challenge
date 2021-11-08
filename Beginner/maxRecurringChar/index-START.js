f9b2b99a086bdefef3e32f9c44c762030b9248a0
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-11-08 18:03:31 -0500

    were over 10 at this point

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -3,9 +3,31 @@ Given a string of text, find and return the most recurring character.
 e.g maxRecurringChar('aabacada') // will return 'a'
 */
 
+//DOES NOT CARE ABOUT CAPITAL LETTER
 function maxRecurringChar(text) {
-  let newWord = text.split('');
-  let set = new Set(newWord);
+  let arr = text.split('');
+  let textObj = arr
+    .map(el => el.toString())
+    .reduce((allLetters, letters) => {
+      if (letters in allLetters) {
+        allLetters[letters]++;
+      } else {
+        allLetters[letters] = 1;
+      }
+      return allLetters;
+    }, {});
+  console.log(textObj);
+  let count = 0;
+  let bigLetter = '';
+  for (let letter in textObj) {
+    if (textObj[letter] > count) {
+      count = textObj[letter];
+      bigLetter = letter;
+    }
+  }
+  console.log('hi');
+  console.log(bigLetter);
+  return bigLetter;
 }
 
 module.exports = maxRecurringChar;
